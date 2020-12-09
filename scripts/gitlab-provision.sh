82fb2eb73f2f079b2ad72b889ed3ae4a746461cd
doja-doja <doreen.jakob@gmx.de>
2020-12-09 12:59:44 +0100

    Provisio.sh: add-apt-repository korrigiert

--- scripts/gitlab-provision.sh
+++ scripts/gitlab-provision.sh
@@ -2,9 +2,9 @@
 # Hier kommt Bockwurst rein
 apt-get update
 apt-get -y upgrade
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io 
 usermod -aG docker vagrant
